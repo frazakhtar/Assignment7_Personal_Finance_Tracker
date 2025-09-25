ff52052437e8c7a99016d858fa21c472339e7ada
frazakhtar <87436611+frazakhtar@users.noreply.github.com>
2025-09-25 20:11:33 +0530

    eslint config and rules added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react'
 function App() {
   return (
     <div className="App">
-    <h1>Test</h1>
+      <h1>Test</h1>
+      <ul>
+        <li>dummy list</li>
+        <li>Eslint check</li>
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
